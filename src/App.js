d48cbc62a477032aa763d3fd3791dc63923342ac
SobinNoBep <tvsobin@gmail.com>
2024-03-07 12:48:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './compontnents/profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+  
 
 function App() {
   return (
+  
     <div className="App">
+        
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src="https://wall.vn/wp-content/uploads/2020/04/anh-dep-viet-nam-2.jpg" />    
+       
+        <Profile/>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App;   
